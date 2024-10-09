90f5cbe4be59b4f77709875020f0768fb42f587c
mahesheninrac <mahesh.solanke@eninrac.com>
2024-10-09 17:31:32 +0530

    updated model

---
+++ backend/model/transactionModel.js
@@ -0,0 +1,44 @@
+const transactionsSchema = new database.Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        require: true,
+    },
+    description: {
+        type: String,
+        require: true,
+        trim: true
+    },
+    category: {
+        type: String,
+        require: true,
+        lowercase: true,
+        trim: true
+    },
+    image: {
+        type: String,
+        require: true,
+        lowercase: true,
+        trim: true
+    },
+    sold: {
+        type: Boolean,
+        default: false,
+    },
+    dateOfSale: {
+        type: Date,
+        require: true,
+    }
+});
+
+const transactionsModel = database.model("transactions", transactionsSchema)
+
+module.exports = transactionsModel
